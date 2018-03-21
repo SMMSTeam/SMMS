ce9ea873db9ab72029c56589ec56bf24b28ea8e5
141250015 <141250015@smail.nju.edu.cn>
2018-03-21 16:35:34 +0800

    测试

--- src/main/java/com/example/demo/controller/StaffController.java
+++ src/main/java/com/example/demo/controller/StaffController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/staff")
 public class StaffController {
 	//测试 again
+	//崩溃
 	public void add(){
 		System.out.println("ces");
 	}
