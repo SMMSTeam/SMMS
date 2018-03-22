2a687b883eae99c4eb9dab530df77424b7bc5671
adikejojo <hcy19960516@163.com>
2018-03-22 17:10:09 +0800

    add repertory method

--- src/main/java/com/example/demo/controller/RepertoryController.java
+++ src/main/java/com/example/demo/controller/RepertoryController.java
@@ -1,16 +1,29 @@
 package com.example.demo.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/repertory")
 public class RepertoryController {
-	public String createInboundBill() {
-		return "/";
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@RequestMapping("/inbound")
+	public String createInboundBill(Model model) {
+		
+		return "inbound";
 	}
-	public String createOutboundBill() {
-		return "/outbound";
+	
+	@RequestMapping("/outbound")
+	public String createOutboundBill(Model model) {
+		return "outbound";
+	}
+	@RequestMapping("/modify")
+	public String createModifyBill(Model model) {
+		return "modify";
+		
 	}
 
 }
