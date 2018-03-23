f9a50caa5b6e6901e897f2ee6bfdc7afdb0fd244
141250015 <141250015@smail.nju.edu.cn>
2018-03-23 15:20:45 +0800

    添加实体类，AccountMessage,StaffMessage,StoreMessage

--- src/main/java/com/example/demo/controller/StaffController.java
+++ src/main/java/com/example/demo/controller/StaffController.java
@@ -1,14 +1,34 @@
 package com.example.demo.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.StaffMessage;
+import com.example.demo.service.StaffService;
+/**
+ * 
+* @ClassName: StaffController
+* @Description:控制器
+* @author 陈亚军
+* @date 2018年3月23日 上午8:47:03
+*
+ */
 
 @Controller
 @RequestMapping("/staff")
 public class StaffController {
-	// 
-	public void add(){
+	@Resource
+	private StaffService staffService;
+	
+	@RequestMapping("/addStaff")
+	public String addStaff(StaffMessage staffManagement){
+		
+		return " ";
 		
 	}
+	
 
 }
--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,10 +1,25 @@
 package com.example.demo.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.StoreService;
+/**
+ * 
+* @ClassName: StoreController
+* @Description: store控制器
+* @author 陈亚军
+* @date 2018年3月23日 下午2:14:57
+*
+ */
 
 @Controller
 @RequestMapping("/store")
 public class StoreController {
+	
+	@Resource
+	private StoreService storeService;
 
 }
