e969bd6015fbb08465c206b300e81a60e13e138c
adikejojo <hcy19960516@163.com>
2018-03-27 17:15:50 +0800

    add dao

---
+++ src/main/java/com/example/demo/dao/OutboundBillDao.java
@@ -0,0 +1,7 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.OutboundBill;
+
+public interface OutboundBillDao {
+	public boolean createoutboundBill(OutboundBill outboundBill);
+}
